35e287686ffca5cfaf1f3c94e92e9d746f16d4c7
Anja Greifenberg <anja.gberg@gmx.de>
2022-02-15 15:36:56 +0100

    all working but ugly

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do |i|
+  Article.create(
+    title: Faker::Hipster.sentence(word_count: 3),
+    content: Faker::Hipster.paragraph
+  )
+end
+
+puts 'Articles created'
